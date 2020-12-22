0ec9e3862136a203b580ac046e41a7ba5f1bc76d
Steve Kim <kimsteve31@gmail.com>
2020-12-22 18:33:58 -0500

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header-name">Weather App</h1>
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default Header;
